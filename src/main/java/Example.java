a41aa2e358f2665b20dc57df99733bd8723d98cb
CristianOquendo <cristian2354g@gmail.com>
2025-04-25 20:25:54 -0500

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Example {
+    public int isPositive(int n) {
+        if (n == 0) {
+            return -1;
+        }
+        if (n > 0) {
+            return 1;
+        }
+        return 0;
+    }
 }
